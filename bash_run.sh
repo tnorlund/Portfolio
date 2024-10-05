a151458e764f7222bedcec9f40764e68f4d3b534
Tyler Norlund <tnorlund@icloud.com>
2024-10-05 14:39:17 -0700

    Got the bash script to run

--- bash_run.sh
+++ bash_run.sh
@@ -7,21 +7,12 @@ IMAGE_DIR="pic/"
 echo "Listing images in $IMAGE_DIR:"
 ls "$IMAGE_DIR"*.{jpg,jpeg,png,gif} 2>/dev/null
 
-# Iterate over each image and use tesseract to write the data to the /out directory
-# for image in "$IMAGE_DIR"*.{jpg,jpeg,png,gif}; do
-#     if [[ -f "$image" ]]; then
-#         # echo "Processing $image"
-#         # output_file="/out/$(basename "$image" | sed 's/\.[^.]*$//')"
-#         # echo "Writing to $output_file"
-#         # tesseract "$image" "$output_file" -c tessedit_create_hocr=0 makebox -l eng
-#     fi
-# done
-
-# remove all spaces from the files in the /pic directory
-for file in "$IMAGE_DIR"*.{jpg,jpeg,png,gif}; do
-    if [[ -f "$file" ]]; then
-        echo "Processing $file"
-        new_file=$(echo "$file" | tr -d ' ')
-        mv "$file" "$new_file"
+Iterate over each image and use tesseract to write the data to the /out directory
+for image in "$IMAGE_DIR"*.{jpg,jpeg,png,gif}; do
+    if [[ -f "$image" ]]; then
+        echo "Processing $image"
+        output_file="out/$(basename "$image" | sed 's/\.[^.]*$//')"
+        echo "Writing to $output_file"
+        tesseract "$image" "$output_file" -c tessedit_create_hocr=0 makebox
     fi
 done
