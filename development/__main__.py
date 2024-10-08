be22152136ab9de1e014ad876492ec466cbc5b34
Tyler Norlund <tnorlund@icloud.com>
2024-10-07 20:00:25 -0700

    🧹 Cleaning IAC static site

--- development/__main__.py
+++ development/__main__.py
@@ -1,6 +1,6 @@
 import pulumi
 import pulumi_aws as aws
-import json
+import s3_website  # Import the new file
 
 # The DynamoDB table
 dynamodb_table = aws.dynamodb.Table(
@@ -32,88 +32,6 @@ dynamodb_table = aws.dynamodb.Table(
 
 pulumi.export("table_name", dynamodb_table.name)
 pulumi.export("region", aws.config.region)
-
-
-# Create an S3 bucket with website configuration
-site_bucket = aws.s3.Bucket("siteBucket",
-    website={
-        "indexDocument": "index.html",
-        "errorDocument": "error.html",
-    }
-)
-
-# Upload index.html and error.html to the bucket
-index_html = aws.s3.BucketObject("index.html",
-    bucket=site_bucket.id,
-    source=pulumi.FileAsset("index.html"),  # Assumes you have an index.html file in your project directory
-    content_type="text/html"
-)
-
-error_html = aws.s3.BucketObject("error.html",
-    bucket=site_bucket.id,
-    source=pulumi.FileAsset("error.html"),  # Assumes you have an error.html file in your project directory
-    content_type="text/html"
-)
-
-# Define a bucket policy to make the bucket objects publicly readable
-bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
-    bucket=site_bucket.bucket,
-    policy=site_bucket.bucket.apply(
-        lambda bucket_name: json.dumps({
-            "Version": "2012-10-17",
-            "Statement": [{
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": f"arn:aws:s3:::{bucket_name}/*",
-            }],
-        })
-    )
-)
-
-# Configure the BucketPublicAccessBlock to allow public policies
-public_access_block = aws.s3.BucketPublicAccessBlock("publicAccessBlock",
-    bucket=site_bucket.id,
-    block_public_acls=False,
-    ignore_public_acls=False,
-    block_public_policy=False,
-    restrict_public_buckets=False
-)
-
-# Create a CloudFront distribution
-cdn = aws.cloudfront.Distribution("cdn",
-    origins=[{
-        "domainName": site_bucket.bucket_regional_domain_name,
-        "originId": site_bucket.id,
-    }],
-    enabled=True,
-    default_root_object="index.html",
-    default_cache_behavior={
-        "allowedMethods": ["GET", "HEAD"],
-        "cachedMethods": ["GET", "HEAD"],
-        "targetOriginId": site_bucket.id,
-        "viewerProtocolPolicy": "redirect-to-https",
-        "forwardedValues": {
-            "cookies": {"forward": "none"},
-            "queryString": False,
-        },
-        "minTtl": 0,
-        "defaultTtl": 3600,
-        "maxTtl": 86400,
-    },
-    price_class="PriceClass_100",
-    restrictions={
-        "geoRestriction": {
-            "restrictionType": "none",
-        },
-    },
-    viewer_certificate={
-        "cloudfrontDefaultCertificate": True,
-    }
-)
-
-pulumi.export('websiteUrl', site_bucket.website_endpoint)
-pulumi.export('cdnUrl', cdn.domain_name)
 
 # open template readme and read contents into stack output
 with open("./Pulumi.README.md") as f:
