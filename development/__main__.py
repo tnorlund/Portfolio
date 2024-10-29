ca171b91c082be7f44cf11a59cebe0a1363b09f8
Tyler Norlund <tnorlund@icloud.com>
2024-10-29 14:35:28 -0700

    🧹 Cleaning up

--- development/__main__.py
+++ development/__main__.py
@@ -1,40 +1,9 @@
 import pulumi
 import pulumi_aws as aws
 import s3_website  # noqa: F401
-import os
-import subprocess
-import shutil
+from dynamo_db import dynamodb_table
 import json
 
-# The DynamoDB table
-dynamodb_table = aws.dynamodb.Table(
-    "GHActionTable",
-    attributes=[
-        aws.dynamodb.TableAttributeArgs(
-            name="PK",
-            type="S",
-        ),
-        aws.dynamodb.TableAttributeArgs(
-            name="SK",
-            type="S",
-        ),
-    ],
-    hash_key="PK",
-    range_key="SK",
-    billing_mode="PAY_PER_REQUEST",
-    ttl=aws.dynamodb.TableTtlArgs(
-        attribute_name="TimeToLive",
-        enabled=True,
-    ),
-    stream_enabled=True,
-    stream_view_type="NEW_IMAGE",
-    tags={
-        "Environment": "dev",
-        "Name": "GHActionTable",
-    },
-)
-
-pulumi.export("table_name", dynamodb_table.name)
 pulumi.export("region", aws.config.region)
 
 # Define the IAM role for the Lambda function with permissions for the DynamoDB table
