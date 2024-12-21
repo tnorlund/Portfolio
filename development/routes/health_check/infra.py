8dba56ccd07f8edc0ab087e31262075b7288b9a6
Tyler Norlund <tnorlund@icloud.com>
2024-12-21 11:34:12 -0800

    🧹 Cleaned up the API structuring

---
+++ development/routes/health_check/infra.py
@@ -0,0 +1,99 @@
+import os
+import json
+import pulumi
+import pulumi_aws as aws
+from pulumi import AssetArchive, FileArchive
+# Import the DynamoDB table name from the dynamo_db module
+from dynamo_db import dynamodb_table
+
+# Reference the directory containing index.py
+HANDLER_DIR = os.path.join(os.path.dirname(__file__), "handler")
+# Get the route name from the directory name
+ROUTE_NAME = os.path.basename(os.path.dirname(__file__))
+# Get the DynamoDB table name
+DYNAMODB_TABLE_NAME = dynamodb_table.name
+
+print(f"HANDLER_DIR: {HANDLER_DIR}")
+print(f"ROUTE_NAME: {ROUTE_NAME}")
+
+
+# Define the IAM role for the Lambda function with permissions for the DynamoDB table
+lambda_role = aws.iam.Role(
+    f"api_{ROUTE_NAME}_lambda_role",
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }""",
+)
+
+# Attach the necessary policies to the role
+lambda_policy = aws.iam.Policy(
+    f"api_{ROUTE_NAME}_lambda_policy",
+    description="IAM policy for Lambda to access DynamoDB",
+    policy=dynamodb_table.arn.apply(
+        lambda arn: json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "dynamodb:GetItem",
+                            "dynamodb:PutItem",
+                            "dynamodb:UpdateItem",
+                            "dynamodb:DeleteItem",
+                            "dynamodb:Scan",
+                            "dynamodb:Query",
+                        ],
+                        "Resource": arn,
+                    }
+                ],
+            }
+        )
+    ),
+)
+
+lambda_role_policy_attachment = aws.iam.RolePolicyAttachment(
+    f"api_{ROUTE_NAME}_lambda_policy_attachment", 
+    role=lambda_role.name, policy_arn=lambda_policy.arn
+)
+
+# Attach the necessary policies to the role
+aws.iam.RolePolicyAttachment(
+    f"api_{ROUTE_NAME}_lambda_basic_execution",
+    role=lambda_role.name,
+    policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+)
+
+# Create the Lambda function for the "user" route
+health_check_lambda = aws.lambda_.Function(
+    f"api_{ROUTE_NAME}_GET_lambda",
+    runtime="python3.9",  # or whichever version you prefer
+    role=lambda_role.arn,
+    code=AssetArchive(
+        {
+            ".": FileArchive(HANDLER_DIR),
+        }
+    ),
+    handler="index.handler",  # file_name.function_name
+    environment={
+        "variables": {
+            "DYNAMODB_TABLE_NAME": DYNAMODB_TABLE_NAME,
+        }
+    },
+)
+
+# CloudWatch log group for the Lambda function
+log_group = aws.cloudwatch.LogGroup(
+    f"api_{ROUTE_NAME}_lambda_log_group",
+    retention_in_days=30,
+)
