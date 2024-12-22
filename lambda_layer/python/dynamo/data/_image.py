5db8832e6ad0a762e32a860fa38f15899026cf32
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 14:43:26 -0800

    🧹 Linted all python

--- lambda_layer/python/dynamo/data/_image.py
+++ lambda_layer/python/dynamo/data/_image.py
@@ -10,31 +10,32 @@ class _Image:
     -------
     addImage(image: Image)
         Adds an image to the database.
-    
+
     """
+
     def addImage(self, image: Image):
         """Adds an image to the database
 
         Args:
             image (Image): The image to add to the database
-        
+
         Raises:
             ValueError: When an image with the same ID already
         """
         try:
             self._client.put_item(
                 TableName=self.table_name,
-                Item=image.to_item(), 
-                ConditionExpression="attribute_not_exists(PK)"
+                Item=image.to_item(),
+                ConditionExpression="attribute_not_exists(PK)",
             )
         except ClientError as e:
             raise ValueError(f"Image with ID {image.id} already exists")
-        
+
     def getImage(self, image_id: int) -> Image:
         try:
             response = self._client.get_item(
                 TableName=self.table_name,
-                Key={"PK": {"S": f"IMAGE#{image_id}"}, "SK": {"S": "IMAGE"}}
+                Key={"PK": {"S": f"IMAGE#{image_id}"}, "SK": {"S": "IMAGE"}},
             )
             return itemToImage(response["Item"])
         except KeyError:
