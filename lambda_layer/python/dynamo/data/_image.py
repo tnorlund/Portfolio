bdcdb60643cbc1b5da73cc6f4cad03ac151ee340
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 13:46:47 -0800

    🔨 Can now add image to DB

--- lambda_layer/python/dynamo/data/_image.py
+++ lambda_layer/python/dynamo/data/_image.py
@@ -3,17 +3,29 @@ from botocore.exceptions import ClientError
 
 
 class _Image:
+    """
+    A class used to represent an Image in the database.
+
+    Methods
+    -------
+    addImage(image: Image)
+        Adds an image to the database.
+    
+    """
     def addImage(self, image: Image):
         """Adds an image to the database
 
         Args:
             image (Image): The image to add to the database
+        
+        Raises:
+            ValueError: When an image with the same ID already
         """
-        print(f"Image: {image}")
         try:
-            self._table.put_item(
-                Item=image.to_item(), ConditionExpression="attribute_not_exists(PK)"
+            self._client.put_item(
+                TableName=self.table_name,
+                Item=image.to_item(), 
+                ConditionExpression="attribute_not_exists(PK)"
             )
         except ClientError as e:
-            print(f"Error adding image: {e}")
-        pass
+            raise ValueError(f"Image with ID {image.id} already exists")
