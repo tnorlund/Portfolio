034e4e3408096453ca1c331478b11330893021d8
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 15:23:09 -0800

    🔨 Added additional attributes for the Image item

--- lambda_layer/python/test/integration/test__image.py
+++ lambda_layer/python/test/integration/test__image.py
@@ -10,7 +10,12 @@ def test_add_image(dynamodb_table: Literal["MyMockedTable"]):
     image_id = 1
     image_width = 10
     image_height = 20
-    image = Image(image_id, image_width, image_height)
+    timestamp_added = "2021-01-01T00:00:00"
+    s3_bucket = "bucket"
+    s3_key = "key"
+    image = Image(
+        image_id, image_width, image_height, timestamp_added, s3_bucket, s3_key
+    )
 
     # Act
     client.addImage(image)
@@ -29,11 +34,14 @@ def test_add_image_error(dynamodb_table: Literal["MyMockedTable"]):
     image_id = 1
     image_width = 10
     image_height = 20
-    image = Image(image_id, image_width, image_height)
+    timestamp_added = "2021-01-01T00:00:00"
+    s3_bucket = "bucket"
+    s3_key = "key"
+    image = Image(image_id, image_width, image_height, timestamp_added, s3_bucket, s3_key)
 
     # Act
     client.addImage(image)
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         client.addImage(image)
 
 
@@ -43,7 +51,10 @@ def test_get_image(dynamodb_table: Literal["MyMockedTable"]):
     image_id = 1
     image_width = 10
     image_height = 20
-    image = Image(image_id, image_width, image_height)
+    timestamp_added = "2021-01-01T00:00:00"
+    s3_bucket = "bucket"
+    s3_key = "key"
+    image = Image(image_id, image_width, image_height, timestamp_added, s3_bucket, s3_key)
     client.addImage(image)
 
     # Act
@@ -52,14 +63,12 @@ def test_get_image(dynamodb_table: Literal["MyMockedTable"]):
     # Assert
     assert retrieved_image == image
 
+
 def test_listImages(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    image_id = 1
-    image_width = 10
-    image_height = 20
-    image_1 = Image(1, 10, 20)
-    image_2 = Image(2, 30, 40)
+    image_1 = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key1")
+    image_2 = Image(2, 30, 40, "2021-01-01T00:00:00", "bucket", "key2")
     client.addImage(image_1)
     client.addImage(image_2)
 
