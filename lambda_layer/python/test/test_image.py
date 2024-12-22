b15fc3fc8b5c377f47e1353c6ed8b738bc34da31
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 10:41:49 -0800

    🔨 Finished adding the image entity

--- lambda_layer/python/test/test_image.py
+++ lambda_layer/python/test/test_image.py
@@ -1,7 +1,9 @@
 import pytest
 from dynamo import Image
 
+
 def test_init():
+    """Test the Image constructor"""
     with pytest.raises(ValueError):
         Image(0, 0.5, 0.5)
     with pytest.raises(ValueError):
@@ -12,3 +14,30 @@ def test_init():
     assert image.id == 1
     assert image.width == 0.5
     assert image.height == 0.5
+
+
+def test_key():
+    """Test the Image.key() method"""
+    image = Image(1, 0.5, 0.5)
+    assert image.key() == {"PK": {"S": "IMAGE#1"}, "SK": {"S": "IMAGE"}}
+
+
+def test_to_item():
+    """Test the Image.to_item() method"""
+    image = Image(1, 0.5, 0.5)
+    assert image.to_item() == {
+        "PK": {"S": "IMAGE#1"},
+        "SK": {"S": "IMAGE"},
+        "Width": {"N": 0.5},
+        "Height": {"N": 0.5},
+    }
+
+def test_repr():
+    """Test the Image.__repr__() method"""
+    image = Image(1, 0.5, 0.5)
+    assert repr(image) == "Image(id=1, width=0.5, height=0.5)"
+
+def test_iter():
+    """Test the Image.__iter__() method"""
+    image = Image(1, 0.5, 0.5)
+    assert dict(image) == {"id": 1, "width": 0.5, "height": 0.5}
