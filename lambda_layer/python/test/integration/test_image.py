fd1f8fc095e7d1f8196e2439890e16795d4c11d9
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 13:46:47 -0800

    🔨 Can now add image to DB

--- lambda_layer/python/test/integration/test_image.py
+++ lambda_layer/python/test/integration/test_image.py
@@ -21,3 +21,16 @@ def test_add_image(dynamodb_table: Literal["MyMockedTable"]):
         Key={"PK": {"S": f"IMAGE#{image_id}"}, "SK": {"S": "IMAGE"}},
     )
     assert response["Item"] == image.to_item()
+
+def test_add_image_error(dynamodb_table: Literal["MyMockedTable"]):
+    # Arrange
+    client = DynamoClient(dynamodb_table)
+    image_id = 1
+    image_width = 10
+    image_height = 20
+    image = Image(image_id, image_width, image_height)
+
+    # Act
+    client.addImage(image)
+    with pytest.raises(Exception):
+        client.addImage(image)
