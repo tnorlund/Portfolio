ca49bbe3a97b293f1e5c8a4581cde83c1af59691
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 15:23:09 -0800

    🔨 Added additional attributes for the Image item

--- lambda_layer/python/test/unit/test_image.py
+++ lambda_layer/python/test/unit/test_image.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 from dynamo import Image, itemToImage
 
@@ -5,56 +6,78 @@ from dynamo import Image, itemToImage
 def test_init():
     """Test the Image constructor"""
     with pytest.raises(ValueError):
-        Image(0, 0.5, 0.5)
+        Image(0, 0.5, 0.5, "2021-01-01T00:00:00", "bucket", "key")
     with pytest.raises(ValueError):
-        Image(1, -0.5, 0.5)
+        Image(1, -0.5, 0.5, "2021-01-01T00:00:00", "bucket", "key")
     with pytest.raises(ValueError):
-        Image(1, 0.5, 1.5)
-    image = Image(1, 10, 20)
-    assert image.id == 1
+        Image(1, 0.5, 1.5, "2021-01-01T00:00:00", "bucket", "key")
+    with pytest.raises(ValueError):
+        Image(1, 0.5, 0.5, 42, "bucket", "key")
+    image = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
+    assert int(image.id) == 1
     assert image.width == 10
     assert image.height == 20
+    assert image.timestamp_added == "2021-01-01T00:00:00"
+    assert image.s3_bucket == "bucket"
+    assert image.s3_key == "key"
+    image = Image(1, 10, 20, datetime(2021, 1, 1, 0, 0, 0, 0), "bucket", "key")
+    assert int(image.id) == 1
+    assert image.width == 10
+    assert image.height == 20
+    assert image.timestamp_added == "2021-01-01T00:00:00"
+    assert image.s3_bucket == "bucket"
+    assert image.s3_key == "key"
 
 
 def test_key():
     """Test the Image.key() method"""
-    image = Image(1, 10, 20)
-    assert image.key() == {"PK": {"S": "IMAGE#1"}, "SK": {"S": "IMAGE"}}
+    image = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
+    assert image.key() == {"PK": {"S": "IMAGE#00001"}, "SK": {"S": "IMAGE"}}
 
 
 def test_to_item():
     """Test the Image.to_item() method"""
-    image = Image(1, 10, 20)
+    image = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
     assert image.to_item() == {
-        "PK": {"S": "IMAGE#1"},
+        "PK": {"S": "IMAGE#00001"},
         "SK": {"S": "IMAGE"},
         "Width": {"N": "10"},
         "Height": {"N": "20"},
+        "TimestampAdded": {"S": "2021-01-01T00:00:00"},
+        "S3Bucket": {"S": "bucket"},
+        "S3Key": {"S": "key"},
     }
 
 
 def test_repr():
     """Test the Image.__repr__() method"""
-    image = Image(1, 10, 20)
-    assert repr(image) == "Image(id=1, width=10, height=20)"
+    image = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
+    assert repr(image) == "Image(id=1, s3_key=key)"
 
 
 def test_iter():
     """Test the Image.__iter__() method"""
-    image = Image(1, 10, 20)
-    assert dict(image) == {"id": 1, "width": 10, "height": 20}
+    image = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
+    assert dict(image) == {
+        "id": 1,
+        "width": 10,
+        "height": 20,
+        "timestamp_added": "2021-01-01T00:00:00",
+        "s3_bucket": "bucket",
+        "s3_key": "key",
+    }
 
 
 def test_eq():
     """Test the Image.__eq__() method"""
-    image1 = Image(1, 10, 20)
-    image2 = Image(1, 10, 20)
+    image1 = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
+    image2 = Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
     assert image1 == image2
-    image3 = Image(2, 10, 20)
+    image3 = Image(2, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
     assert image1 != image3
-    image4 = Image(1, 20, 20)
+    image4 = Image(1, 20, 20, "2021-01-01T00:00:00", "bucket", "key")
     assert image1 != image4
-    image5 = Image(1, 10, 30)
+    image5 = Image(1, 10, 30, "2021-01-01T00:00:00", "bucket", "key")
     assert image1 != image5
     assert image1 != 42
 
@@ -66,9 +89,12 @@ def test_itemToImage():
         "SK": {"S": "IMAGE"},
         "Width": {"N": "10"},
         "Height": {"N": "20"},
+        "TimestampAdded": {"S": "2021-01-01T00:00:00"},
+        "S3Bucket": {"S": "bucket"},
+        "S3Key": {"S": "key"},
     }
     image = itemToImage(item)
-    assert image == Image(1, 10, 20)
+    assert image == Image(1, 10, 20, "2021-01-01T00:00:00", "bucket", "key")
     item = {
         "PK": {"S": "IMAGE#1"},
         "SK": {"S": "IMAGE"},
@@ -94,6 +120,8 @@ def test_itemToImage():
         "SK": {"S": "IMAGE"},
         "Width": {"N": "10"},
         "Height": {"N": "20"},
+        "TimestampAdded": {"S": "2021-01-01T00:00:00"},
+        "S3Bucket": {"S": "bucket"},
         "Extra": {"S": "Extra"},
     }
     with pytest.raises(ValueError):
@@ -103,6 +131,8 @@ def test_itemToImage():
         "SK": {"S": "IMAGE"},
         "Width": {"N": "10"},
         "Height": {"N": "20"},
+        "TimestampAdded": {"S": "2021-01-01T00:00:00"},
+        "S3Key": {"S": "key"},
         "Extra": {"S": "Extra"},
     }
     with pytest.raises(ValueError):
@@ -112,15 +142,9 @@ def test_itemToImage():
         "SK": {"S": "IMAGE"},
         "Width": {"N": "10"},
         "Height": {"N": "20"},
-        "Extra": {"S": "Extra"},
-    }
-    with pytest.raises(ValueError):
-        itemToImage(item)
-    item = {
-        "PK": {"S": "IMAGE#1"},
-        "SK": {"S": "IMAGE"},
-        "Width": {"N": "10"},
-        "Height": {"N": "20"},
+        "TimestampAdded": {"S": "2021-01-01T00:00:00"},
+        "S3Bucket": {"S": "bucket"},
+        "S3Key": {"S": "key"},
         "Extra": {"S": "Extra"},
     }
     with pytest.raises(ValueError):
