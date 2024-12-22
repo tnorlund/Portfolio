048df05720b1f34e674a00e4eecb1e3cccc73697
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 10:41:49 -0800

    🔨 Finished adding the image entity

--- lambda_layer/python/dynamo/entities/image.py
+++ lambda_layer/python/dynamo/entities/image.py
@@ -1,3 +1,6 @@
+from typing import Generator, Tuple
+
+
 class Image:
     def __init__(self, id: int, width: int, height: int):
         """Constructs a new Image object for DynamoDB
@@ -16,3 +19,37 @@ class Image:
         self.id = id
         self.width = width
         self.height = height
+
+    def key(self) -> dict:
+        """Generates the primary key for the image
+
+        Returns:
+            str: The primary key for the image
+        """
+        return {"PK": {"S": f"IMAGE#{self.id}"}, "SK": {"S": "IMAGE"}}
+
+    def to_item(self) -> dict:
+        """Converts the Image object to a DynamoDB item
+
+        Returns:
+            dict: The DynamoDB item representation of the Image
+        """
+        return {**self.key(), "Width": {"N": self.width}, "Height": {"N": self.height}}
+
+    def __repr__(self) -> str:
+        """Returns a string representation of the Image object
+
+        Returns:
+            str: The string representation of the Image object
+        """
+        return f"Image(id={self.id}, width={self.width}, height={self.height})"
+
+    def __iter__(self) -> Generator[Tuple[str, int], None, None]:
+        """Returns an iterator over the Image object
+
+        Returns:
+            dict: The iterator over the Image object
+        """
+        yield "id", self.id
+        yield "width", self.width
+        yield "height", self.height
