ce07f7256d962cd95d34875f1579dc9bf8af2036
Tyler Norlund <tnorlund@icloud.com>
2024-12-22 14:43:26 -0800

    🧹 Linted all python

--- lambda_layer/python/dynamo/data/dynamo_client.py
+++ lambda_layer/python/dynamo/data/dynamo_client.py
@@ -4,7 +4,19 @@ from dynamo.entities import Image
 
 
 class DynamoClient(_Image):
+    """A class used to represent a DynamoDB client."""
+
     def __init__(self, table_name: str, region: str = "us-east-1"):
+        """Initializes a DynamoClient instance.
+
+        Args:
+            table_name (str): The name of the DynamoDB table.
+            region (str, optional): The AWS region where the DynamoDB table is located. Defaults to "us-east-1".
+
+        Attributes:
+            _client (boto3.client): The Boto3 DynamoDB client.
+            table_name (str): The name of the DynamoDB table.
+        """
+
         self._client = boto3.client("dynamodb", region_name=region)
         self.table_name = table_name
-
