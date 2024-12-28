9b514ec70a6a0ba5af47ecb5c92413702b558abe
Tyler Norlund <tnorlund@icloud.com>
2024-12-28 11:47:31 -0800

    🔨 API up and running

--- development/api_gateway.py
+++ development/api_gateway.py
@@ -2,6 +2,7 @@ import pulumi
 import pulumi_aws as aws
 # Import the different routes
 from routes.health_check.infra import health_check_lambda
+from routes.images.infra import images_lambda
 
 api = aws.apigatewayv2.Api(
     "my-api",
@@ -9,7 +10,7 @@ api = aws.apigatewayv2.Api(
 )
 
 # /health_check
-integration = aws.apigatewayv2.Integration(
+integration_health_check = aws.apigatewayv2.Integration(
     "health_check_lambda_integration",
     api_id=api.id,
     integration_type="AWS_PROXY",
@@ -17,17 +18,41 @@ integration = aws.apigatewayv2.Integration(
     integration_method="POST",
     payload_format_version="2.0",
 )
-route = aws.apigatewayv2.Route(
+route_health_check = aws.apigatewayv2.Route(
     "health_check_route",
     api_id=api.id,
     route_key="GET /health_check",
-    target=integration.id.apply(lambda id: f"integrations/{id}"),
+    target=integration_health_check.id.apply(lambda id: f"integrations/{id}"),
     opts=pulumi.ResourceOptions(replace_on_changes=["route_key", "target"]),
 )
-lambda_permission = aws.lambda_.Permission(
-    "lambda_permission",
+lambda_permission_health_check = aws.lambda_.Permission(
+    "health_check_lambda_permission",
     action="lambda:InvokeFunction",
     function=health_check_lambda.name,
+    principal="apigateway.amazonaws.com",
+    source_arn=api.execution_arn.apply(lambda arn: f"{arn}/*/*"),
+)
+
+# /images
+integration_images = aws.apigatewayv2.Integration(
+    "images_lambda_integration",
+    api_id=api.id,
+    integration_type="AWS_PROXY",
+    integration_uri=images_lambda.invoke_arn,
+    integration_method="POST",
+    payload_format_version="2.0",
+)
+route_images = aws.apigatewayv2.Route(
+    "images_route",
+    api_id=api.id,
+    route_key="GET /images",
+    target=integration_images.id.apply(lambda id: f"integrations/{id}"),
+    opts=pulumi.ResourceOptions(replace_on_changes=["route_key", "target"]),
+)
+lambda_permission_images = aws.lambda_.Permission(
+    "images_lambda_permission",
+    action="lambda:InvokeFunction",
+    function=images_lambda.name,
     principal="apigateway.amazonaws.com",
     source_arn=api.execution_arn.apply(lambda arn: f"{arn}/*/*"),
 )
@@ -45,10 +70,15 @@ stage = aws.apigatewayv2.Stage(
     name="$default",
     route_settings=[
         {
-            "routeKey": route.route_key,
+            "routeKey": route_health_check.route_key,
             "throttlingBurstLimit": 5000,
             "throttlingRateLimit": 10000,
-        }
+        },
+        {
+            "routeKey": route_images.route_key,
+            "throttlingBurstLimit": 5000,
+            "throttlingRateLimit": 10000,
+        },
     ],
     auto_deploy=True,
     access_log_settings=aws.apigatewayv2.StageAccessLogSettingsArgs(
--- development/routes/images/infra.py
+++ development/routes/images/infra.py
@@ -4,10 +4,18 @@ import pulumi
 import pulumi_aws as aws
 from pulumi import AssetArchive, FileArchive
 
+# Import the DynamoDB table name from the dynamo_db module
+from dynamo_db import dynamodb_table
+
+# Import the Lambda Layer from the lambda_layer module
+from lambda_layer import lambda_layer
+
 # Reference the directory containing index.py
 HANDLER_DIR = os.path.join(os.path.dirname(__file__), "handler")
 # Get the route name from the directory name
 ROUTE_NAME = os.path.basename(os.path.dirname(__file__))
+# Get the DynamoDB table name
+DYNAMODB_TABLE_NAME = dynamodb_table.name
 
 
 # Define the IAM role for the Lambda function with permissions for the DynamoDB table
@@ -29,6 +37,32 @@ lambda_role = aws.iam.Role(
 )
 
 # Attach the necessary policies to the role
+lambda_policy = aws.iam.Policy(
+    f"api_{ROUTE_NAME}_lambda_policy",
+    description="IAM policy for Lambda to access DynamoDB",
+    policy=dynamodb_table.arn.apply(
+        lambda arn: json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": ["dynamodb:Scan"],
+                        "Resource": arn,
+                    }
+                ],
+            }
+        )
+    ),
+)
+
+lambda_role_policy_attachment = aws.iam.RolePolicyAttachment(
+    f"api_{ROUTE_NAME}_lambda_policy_attachment",
+    role=lambda_role.name,
+    policy_arn=lambda_policy.arn,
+)
+
+# Attach the necessary policies to the role
 aws.iam.RolePolicyAttachment(
     f"api_{ROUTE_NAME}_lambda_basic_execution",
     role=lambda_role.name,
@@ -36,7 +70,7 @@ aws.iam.RolePolicyAttachment(
 )
 
 # Create the Lambda function for the "user" route
-health_check_lambda = aws.lambda_.Function(
+images_lambda = aws.lambda_.Function(
     f"api_{ROUTE_NAME}_GET_lambda",
     runtime="python3.9",  # or whichever version you prefer
     role=lambda_role.arn,
@@ -46,6 +80,12 @@ health_check_lambda = aws.lambda_.Function(
         }
     ),
     handler="index.handler",  # file_name.function_name
+    layers=[lambda_layer.arn],
+    environment={
+        "variables": {
+            "DYNAMODB_TABLE_NAME": DYNAMODB_TABLE_NAME,
+        }
+    },
 )
 
 # CloudWatch log group for the Lambda function
