584aed69db0ffe6022d58ddc11990fa31885acbd
Tyler Norlund <tnorlund@icloud.com>
2025-01-10 15:14:13 -0800

    Adding Additional OCR Data (#1)
    * 🔨 Storing more OCR data for Lines
    
    * 🧹 Linting
    
    * 🔨 Added details to word entity

--- lambda_layer/python/dynamo/entities/word.py
+++ lambda_layer/python/dynamo/entities/word.py
@@ -1,6 +1,39 @@
 from typing import Generator, Tuple
 from decimal import Decimal, ROUND_HALF_UP
 
+def assert_valid_boundingBox(boundingBox):
+    """
+    Assert that the bounding box is valid.
+    """
+    if not isinstance(boundingBox, dict):
+        raise ValueError("boundingBox must be a dictionary")
+    for key in ["x", "y", "width", "height"]:
+        if key not in boundingBox:
+            raise ValueError(f"boundingBox must contain the key '{key}'")
+        if not isinstance(boundingBox[key], (int, float)):
+            raise ValueError(f"boundingBox['{key}'] must be a number")
+    return boundingBox
+
+
+def assert_valid_point(point):
+    """
+    Assert that the point is valid.
+    """
+    if not isinstance(point, dict):
+        raise ValueError("point must be a dictionary")
+    for key in ["x", "y"]:
+        if key not in point:
+            raise ValueError(f"point must contain the key '{key}'")
+        if not isinstance(point[key], (int, float)):
+            raise ValueError(f"point['{key}'] must be a number")
+    return point
+
+
+def map_to_dict(map):
+    """
+    Convert a DynamoDB map to a dictionary.
+    """
+    return {key: float(value["N"]) for key, value in map.items()}
 
 def _format_float(
     value: float, decimal_places: int = 10, total_length: int = 20
@@ -35,11 +68,13 @@ class Word:
         line_id: int,
         id: int,
         text: str,
-        x: float,
-        y: float,
-        width: float,
-        height: float,
-        angle: float,
+        boundingBox: dict,
+        topRight: dict,
+        topLeft: dict,
+        bottomRight: dict,
+        bottomLeft: dict,
+        angleDegrees: float,
+        angleRadians: float,
         confidence: float,
         tags: list[str] = None
     ):
@@ -55,23 +90,24 @@ class Word:
         if not isinstance(text, str):
             raise ValueError("text must be a string")
         self.text = text
-        if not isinstance(x, float):
-            raise ValueError("x must be a float")
-        self.x = x
-        if not isinstance(y, float):
-            raise ValueError("y must be a float")
-        self.y = y
-        if not isinstance(width, float):
-            raise ValueError("width must be a float")
-        self.width = width
-        if not isinstance(height, float):
-            raise ValueError("height must be a float")
-        self.height = height
-        if isinstance(angle, int):
-            angle = float(angle)
-        if not isinstance(angle, float):
-            raise ValueError("angle must be a float or int")
-        self.angle = angle
+        assert_valid_boundingBox(boundingBox)
+        self.boundingBox = boundingBox
+        assert_valid_point(topRight)
+        self.topRight = topRight
+        assert_valid_point(topLeft)
+        self.topLeft = topLeft
+        assert_valid_point(bottomRight)
+        self.bottomRight = bottomRight
+        assert_valid_point(bottomLeft)
+        self.bottomLeft = bottomLeft
+        # Ensure the angleDegree is a float
+        if not isinstance(angleDegrees, float):
+            raise ValueError("angleDegrees must be a float")
+        self.angleDegrees = angleDegrees
+        # Ensure the angleRadians is a float
+        if not isinstance(angleRadians, float):
+            raise ValueError("angleRadians must be a float")
+        self.angleRadians = angleRadians
         if confidence <= 0 or confidence > 1:
             raise ValueError("confidence must be a float between 0 and 1")
         self.confidence = confidence
@@ -90,11 +126,40 @@ class Word:
             **self.key(),
             "Type": {"S": "WORD"},
             "Text": {"S": self.text},
-            "X": {"N": _format_float(self.x, 20, 22)},
-            "Y": {"N": _format_float(self.y, 20, 22)},
-            "Width": {"N": _format_float(self.width, 20, 22)},
-            "Height": {"N": _format_float(self.height, 20, 22)},
-            "Angle": {"N": _format_float(self.angle, 10, 12)},
+            "BoundingBox": {
+                "M": {
+                    "x": {"N": _format_float(self.boundingBox["x"], 18, 20)},
+                    "y": {"N": _format_float(self.boundingBox["y"], 18, 20)},
+                    "width": {"N": _format_float(self.boundingBox["width"], 18, 20)},
+                    "height": {"N": _format_float(self.boundingBox["height"], 18, 20)},
+                }
+            },
+            "TopRight": {
+                "M": {
+                    "x": {"N": _format_float(self.topRight["x"], 18, 20)},
+                    "y": {"N": _format_float(self.topRight["y"], 18, 20)},
+                }
+            },
+            "TopLeft": {
+                "M": {
+                    "x": {"N": _format_float(self.topLeft["x"], 18, 20)},
+                    "y": {"N": _format_float(self.topLeft["y"], 18, 20)},
+                }
+            },
+            "BottomRight": {
+                "M": {
+                    "x": {"N": _format_float(self.bottomRight["x"], 18, 20)},
+                    "y": {"N": _format_float(self.bottomRight["y"], 18, 20)},
+                }
+            },
+            "BottomLeft": {
+                "M": {
+                    "x": {"N": _format_float(self.bottomLeft["x"], 18, 20)},
+                    "y": {"N": _format_float(self.bottomLeft["y"], 18, 20)},
+                }
+            },
+            "AngleDegrees": {"N": _format_float(self.angleDegrees, 10, 12)},
+            "AngleRadians": {"N": _format_float(self.angleRadians, 10, 12)},
             "Confidence": {"N": _format_float(self.confidence, 2, 2)},
         }
         if self.tags:
@@ -110,16 +175,20 @@ class Word:
         return f"Word(id={int(self.id)}, text='{self.text}')"
     
     def __iter__(self) -> Generator[Tuple[str, str], None, None]:
-        yield "PK", f"IMAGE#{self.image_id:05d}"
-        yield "SK", f"LINE#{self.line_id:05d}#WORD#{self.id:05d}"
+        yield "image_id", self.image_id
+        yield "line_id", self.line_id
+        yield "id", self.id
         yield "text", self.text
         yield "tags", self.tags
-        yield "x", _format_float(self.x, 20, 22)
-        yield "y", _format_float(self.y, 20, 22)
-        yield "width", _format_float(self.width, 20, 22)
-        yield "height", _format_float(self.height, 20, 22)
-        yield "angle", _format_float(self.angle, 10, 12)
-        yield "confidence", _format_float(self.confidence, 2, 2)
+        yield "boundingBox", self.boundingBox
+        yield "topRight", self.topRight
+        yield "topLeft", self.topLeft
+        yield "bottomRight", self.bottomRight
+        yield "bottomLeft", self.bottomLeft
+        yield "angleDegrees", self.angleDegrees
+        yield "angleRadians", self.angleRadians
+        yield "tags", self.tags
+        yield "confidence", self.confidence
 
     def __eq__(self, other: object) -> bool:
         """Compares two Word objects
@@ -132,21 +201,20 @@ class Word:
         """
         if not isinstance(other, Word):
             return False
-        return all(
-            getattr(self, attr) == getattr(other, attr)
-            for attr in [
-                "image_id",
-                "line_id",
-                "id",
-                "text",
-                "x",
-                "y",
-                "width",
-                "height",
-                "angle",
-                "confidence",
-                "tags"
-            ]
+        return (
+            self.image_id == other.image_id
+            and self.line_id == other.line_id
+            and self.id == other.id
+            and self.text == other.text
+            and self.boundingBox == other.boundingBox
+            and self.topRight == other.topRight
+            and self.topLeft == other.topLeft
+            and self.bottomRight == other.bottomRight
+            and self.bottomLeft == other.bottomLeft
+            and self.angleDegrees == other.angleDegrees
+            and self.angleRadians == other.angleRadians
+            and self.confidence == other.confidence
+            and self.tags == other.tags
         )
         
     
@@ -164,11 +232,13 @@ def itemToWord(item: dict) -> Word:
         int(item["SK"]["S"].split("#")[1]),
         int(item["SK"]["S"].split("#")[3]),
         item["Text"]["S"],
-        float(item["X"]["N"]),
-        float(item["Y"]["N"]),
-        float(item["Width"]["N"]),
-        float(item["Height"]["N"]),
-        float(item["Angle"]["N"]),
+        map_to_dict(item["BoundingBox"]["M"]),
+        map_to_dict(item["TopRight"]["M"]),
+        map_to_dict(item["TopLeft"]["M"]),
+        map_to_dict(item["BottomRight"]["M"]),
+        map_to_dict(item["BottomLeft"]["M"]),
+        float(item["AngleDegrees"]["N"]),
+        float(item["AngleRadians"]["N"]),
         float(item["Confidence"]["N"]),
         item.get("Tags", {}).get("SS", [])
     )
