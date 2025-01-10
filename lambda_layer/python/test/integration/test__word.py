788af19134b7e024e4f54b39b1da944b937afddc
Tyler Norlund <tnorlund@icloud.com>
2025-01-10 15:14:13 -0800

    Adding Additional OCR Data (#1)
    * 🔨 Storing more OCR data for Lines
    
    * 🧹 Linting
    
    * 🔨 Added details to word entity

--- lambda_layer/python/test/integration/test__word.py
+++ lambda_layer/python/test/integration/test__word.py
@@ -3,22 +3,31 @@ import pytest
 import boto3
 from dynamo import Word, DynamoClient
 
+correct_word_params = {
+    "image_id": 1,
+    "line_id": 2,
+    "id": 3,
+    "text": "07\/03\/2024",
+    "boundingBox": {
+        "y": 0.9167082878750482,
+        "width": 0.08690182470506236,
+        "x": 0.4454263367632384,
+        "height": 0.022867568134581906,
+    },
+    "topRight": {"y": 0.9307722198001792, "x": 0.5323281614683008},
+    "topLeft": {"x": 0.44837726658954413, "y": 0.9395758560096301},
+    "bottomRight": {"y": 0.9167082878750482, "x": 0.529377231641995},
+    "bottomLeft": {"x": 0.4454263367632384, "y": 0.9255119240844992},
+    "angleDegrees": -5.986527,
+    "angleRadians": -0.10448461,
+    "confidence": 1,
+}
+
 
 def test_addWord_no_tags(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word = Word(**correct_word_params)
 
     # Act
     client.addWord(word)
@@ -31,22 +40,11 @@ def test_addWord_no_tags(dynamodb_table: Literal["MyMockedTable"]):
     assert "Item" in response, f"Item not found. response: {response}"
     assert response["Item"] == word.to_item()
 
+
 def test_addWord_with_tags(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-        ["tag1", "tag2"],
-    )
+    word = Word(**correct_word_params, tags=["tag1", "tag2"])
 
     # Act
     client.addWord(word)
@@ -63,51 +61,21 @@ def test_addWord_with_tags(dynamodb_table: Literal["MyMockedTable"]):
 def test_add_word_error(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word = Word(**correct_word_params)
 
     # Act
     client.addWord(word)
     with pytest.raises(ValueError):
         client.addWord(word)
 
+
 def test_addWords(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word1 = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
-    word2 = Word(
-        1,
-        1,
-        2,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word1 = Word(**correct_word_params)
+    word2_params = correct_word_params.copy()
+    word2_params["id"] = 4
+    word2 = Word(**word2_params)
 
     # Act
     client.addWords([word1, word2])
@@ -127,29 +95,20 @@ def test_addWords(dynamodb_table: Literal["MyMockedTable"]):
     assert "Item" in response, f"Item not found. response: {response}"
     assert response["Item"] == word2.to_item()
 
+
 def test_deleteWord(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word = Word(**correct_word_params)
     client.addWord(word)
 
     # Act
-    client.deleteWord(1, 1, 1)
+    client.deleteWord(1, 2, 3)
 
     # Assert
     with pytest.raises(ValueError):
-        client.getWord(1, 1, 1)
+        client.getWord(1, 2, 3)
+
 
 def test_deleteWord_error(dynamodb_table: Literal["MyMockedTable"]):
     """Raises exception when word is not found"""
@@ -158,35 +117,16 @@ def test_deleteWord_error(dynamodb_table: Literal["MyMockedTable"]):
 
     # Act
     with pytest.raises(ValueError):
-        client.deleteWord(1, 1, 1)
+        client.deleteWord(1, 2, 3)
+
 
 def test_deleteWordsFromLine(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word1 = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
-    word2 = Word(
-        1,
-        1,
-        2,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word1 = Word(**correct_word_params)
+    word2_params = correct_word_params.copy()
+    word2_params["id"] = 4
+    word2 = Word(**word2_params)
     client.addWord(word1)
     client.addWord(word2)
 
@@ -199,28 +139,19 @@ def test_deleteWordsFromLine(dynamodb_table: Literal["MyMockedTable"]):
     with pytest.raises(ValueError):
         client.getWord(1, 1, 2)
 
+
 def test_getWord(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word = Word(**correct_word_params)
     client.addWord(word)
 
     # Act
-    retrieved_word = client.getWord(1, 1, 1)
+    retrieved_word = client.getWord(1, 2, 3)
 
     # Assert
     assert retrieved_word == word
+
 
 def test_getWord_error(dynamodb_table: Literal["MyMockedTable"]):
     """Raises exception when word is not found"""
@@ -229,35 +160,16 @@ def test_getWord_error(dynamodb_table: Literal["MyMockedTable"]):
 
     # Act
     with pytest.raises(ValueError):
-        client.getWord(1, 1, 2)
+        client.getWord(1, 2, 3)
+
 
 def test_listWords(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word1 = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
-    word2 = Word(
-        1,
-        1,
-        2,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word1 = Word(**correct_word_params)
+    word2_params = correct_word_params.copy()
+    word2_params["id"] = 4
+    word2 = Word(**word2_params)
     client.addWord(word1)
     client.addWord(word2)
 
@@ -268,51 +180,24 @@ def test_listWords(dynamodb_table: Literal["MyMockedTable"]):
     assert word1 in words
     assert word2 in words
 
+
 def test_listWordsFromLine(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
-    word1 = Word(
-        1,
-        1,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
-    word2 = Word(
-        1,
-        1,
-        2,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
-    word3 = Word(
-        1,
-        2,
-        1,
-        "06\/27\/2024",
-        0.1495695452950324,
-        0.8868912353567051,
-        0.08727867372574347,
-        0.024234482472679675,
-        7.7517295,
-        1,
-    )
+    word1 = Word(**correct_word_params)
+    word2_params = correct_word_params.copy()
+    word2_params["id"] = 4
+    word2 = Word(**word2_params)
+    word3_params = correct_word_params.copy()
+    word3_params["id"] = 5
+    word3_params["line_id"] = 3
+    word3 = Word(**word3_params)
     client.addWord(word1)
     client.addWord(word2)
     client.addWord(word3)
 
     # Act
-    words = client.listWordsFromLine(1, 1)
+    words = client.listWordsFromLine(1, 2)
 
     # Assert
     assert word1 in words
