314c052ab95776500729368b350f9a0083073ad2
Tyler Norlund <tnorlund@icloud.com>
2025-01-09 18:50:31 -0800

    🔨 Can now update image and tag word

--- lambda_layer/python/test/integration/test__word.py
+++ lambda_layer/python/test/integration/test__word.py
@@ -4,7 +4,7 @@ import boto3
 from dynamo import Word, DynamoClient
 
 
-def test_add_word(dynamodb_table: Literal["MyMockedTable"]):
+def test_addWord_no_tags(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
     client = DynamoClient(dynamodb_table)
     word = Word(
@@ -30,6 +30,35 @@ def test_add_word(dynamodb_table: Literal["MyMockedTable"]):
     )
     assert "Item" in response, f"Item not found. response: {response}"
     assert response["Item"] == word.to_item()
+
+def test_addWord_with_tags(dynamodb_table: Literal["MyMockedTable"]):
+    # Arrange
+    client = DynamoClient(dynamodb_table)
+    word = Word(
+        1,
+        1,
+        1,
+        "06\/27\/2024",
+        0.1495695452950324,
+        0.8868912353567051,
+        0.08727867372574347,
+        0.024234482472679675,
+        7.7517295,
+        1,
+        ["tag1", "tag2"],
+    )
+
+    # Act
+    client.addWord(word)
+
+    # Assert
+    response = boto3.client("dynamodb", region_name="us-east-1").get_item(
+        TableName=dynamodb_table,
+        Key=word.key(),
+    )
+    assert "Item" in response, f"Item not found. response: {response}"
+    assert response["Item"] == word.to_item()
+
 
 def test_add_word_error(dynamodb_table: Literal["MyMockedTable"]):
     # Arrange
