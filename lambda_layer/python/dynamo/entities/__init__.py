aaa8a7f83b4b69dc42911f11a8409bd2475f770d
Tyler Norlund <tnorlund@icloud.com>
2025-01-12 09:16:10 -0800

    💯 Passing tests

--- lambda_layer/python/dynamo/entities/__init__.py
+++ lambda_layer/python/dynamo/entities/__init__.py
@@ -2,4 +2,4 @@ from .image import Image, itemToImage
 from .line import Line, itemToLine
 from .word import Word, itemToWord
 from .letter import Letter, itemToLetter
-from .scaled_image import ScaledImage, ItemToScaledImage
+from .receipt import Receipt, itemToReceipt
