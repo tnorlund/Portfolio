0522d4ba06dbb21015f5cdef0b58dcedaaaefcea
Tyler Norlund <tnorlund@icloud.com>
2025-01-13 10:07:58 -0800

    🧹 Moved lambda layer to development dir

--- lambda_layer/python/dynamo/entities/__init__.py
+++
@@ -1,8 +0,0 @@
-from .image import Image, itemToImage
-from .line import Line, itemToLine
-from .word import Word, itemToWord
-from .letter import Letter, itemToLetter
-from .receipt import Receipt, itemToReceipt
-from .receipt_line import ReceiptLine, itemToReceiptLine
-from .receipt_word import ReceiptWord, itemToReceiptWord
-from .receipt_letter import ReceiptLetter, itemToReceiptLetter
