e92a07853373c82ba0dadcc065741e3d52569a8b
Tyler Norlund <tnorlund@icloud.com>
2025-01-19 15:38:20 -0800

    🔨 Added my face

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-import ImageGrid from './ImageGrid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <ImageGrid />
+      <h1>Tyler Norlund</h1>
+      <p>Data Person</p>
+      <img src="/face.jpg" alt="Tyler Norlund" style={{
+            borderRadius: '50%',
+            width: '200px',       // pick whatever size you want
+            height: '200px',      // same as width for a perfect circle
+            objectFit: 'cover',   // ensures the image fully covers the circle
+          }} />
+      {/* <button>Resume</button> */}
       </header>
     </div>
   );
