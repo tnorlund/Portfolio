74510c45f4e02bf376b6d4486fcd9b26414b08c2
Tyler Norlund <tnorlund@icloud.com>
2025-01-23 12:28:18 -0800

    🔨 Renamed dir

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,46 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Resume from './Resume'; // import the Resume component
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          <Link to="/">Tyler Norlund</Link>
+        </h1>
+      </header>
+      <div className="resume-container">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <img
+                src="/face.png"
+                alt="Tyler Norlund"
+                style={{
+                  borderRadius: '50%',
+                  width: '200px',
+                  height: '200px',
+                  objectFit: 'cover',
+                  marginTop: '1rem',
+                }}
+              />
+
+                <button onClick={() => window.location.href = '/resume'}>
+                  Résumé
+                </button>
+            </main>
+          }
+        />
+        
+        {/* Resume route */}
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
