438825bda54cd70ac8c413e58246767bcd0100d6
Tyler Norlund <tnorlund@icloud.com>
2025-01-24 09:36:50 -0800

    🔨

---
+++ portfolio/src/Receipt.tsx
@@ -0,0 +1,13 @@
+import ImageGrid from "./ImageGrid";
+
+function Receipt() {
+  return (
+    <div>
+      <h1>What it is</h1>
+      <p>This is a project</p>
+      <ImageGrid />
+    </div>
+  );
+}
+
+export default Receipt;
