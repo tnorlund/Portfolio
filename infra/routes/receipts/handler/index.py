7c37c8c311d4828a4ff836eab2724ee2e82e8239
Tyler Norlund <tnorlund@icloud.com>
2025-01-24 09:49:55 -0800

    🔨 Added list receipts to API

---
+++ infra/routes/receipts/handler/index.py
@@ -0,0 +1,87 @@
+import os
+import logging
+import json
+from dynamo import DynamoClient  # type: ignore
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Get the environment variables
+dynamodb_table_name = os.environ["DYNAMODB_TABLE_NAME"]
+
+
+def serialize_listReceiptDetails_payload(
+    payload: dict[int, dict[str, object]]
+) -> dict[int, dict[str, object]]:
+    """
+    Convert each Image/Line/Receipt object into a pure dict.
+    """
+    serialized = {}
+    for receipt_id, data in payload.items():
+        serialized[receipt_id] = {}
+
+        # If there's an 'receipt' object, convert it to a dict
+        if "receipt" in data:
+            serialized[receipt_id]["receipt"] = dict(data["receipt"])
+
+        # Convert the list of receipt_words
+        if "words" in data:
+            serialized[receipt_id]["words"] = [dict(line) for line in data["words"]]
+
+        # Convert the list of word tags
+        # if "receipts" in data:
+        #     serialized[receipt_id]["receipts"] = [
+        #         dict(receipt) for receipt in data["receipts"]
+        #     ]
+
+    return serialized
+
+
+def handler(event, _):
+    logger.info("Received event: %s", event)
+    http_method = event["requestContext"]["http"]["method"].upper()
+
+    if http_method == "GET":
+        client = DynamoClient(dynamodb_table_name)
+        query_params = event.get("queryStringParameters") or {}
+        if "limit" not in query_params:
+            payload, last_evaluated_key = client.listReceiptDetails()
+            return {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "payload": serialize_listReceiptDetails_payload(payload),
+                        "last_evaluated_key": last_evaluated_key,
+                    }
+                ),
+            }
+        else:
+            limit = int(query_params["limit"])
+            if "last_evaluated_key" in query_params:
+                last_evaluated_key = json.loads(query_params["last_evaluated_key"])
+                payload, last_evaluated_key = client.listReceiptDetails(
+                    limit, last_evaluated_key
+                )
+                return {
+                    "statusCode": 200,
+                    "body": json.dumps(
+                        {
+                            "payload": serialize_listReceiptDetails_payload(payload),
+                            "last_evaluated_key": last_evaluated_key,
+                        }
+                    ),
+                }
+            payload, last_evaluated_key = client.listImageDetails(limit)
+            return {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "payload": serialize_listReceiptDetails_payload(payload),
+                        "last_evaluated_key": last_evaluated_key,
+                    }
+                ),
+            }
+    elif http_method == "POST":
+        return {"statusCode": 405, "body": "Method not allowed"}
+    else:
+        return {"statusCode": 405, "body": f"Method {http_method} not allowed"}
