7d35770a406edbdbf08aedd8ffd760c7824398e4
Tyler Norlund <tnorlund@icloud.com>
2025-01-29 20:09:08 -0800

    💯 GH Action test

--- infra/lambda_layer/python/setup.py
+++ infra/lambda_layer/python/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="dynamo",
     version="1",
-    packages=find_packages(where="dynamo"),
-    package_dir={"": "dynamo"},
+    packages=find_packages(),
+    # package_dir={"": "dynamo"},
     install_requires=["requests"],
     extras_require={
         "test": [
