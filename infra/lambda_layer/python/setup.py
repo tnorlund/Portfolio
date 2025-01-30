a0c2a93c73a07b9590ddf6635ccdc92b27bbba3b
Tyler Norlund <tnorlund@icloud.com>
2025-01-29 18:29:47 -0800

    💯 Added pytest markers for integration and unit tests

--- infra/lambda_layer/python/setup.py
+++ infra/lambda_layer/python/setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="dynamo",
     version="1",
-    packages=find_packages(),
-    install_requires=[
-        # Add your dependencies here
-    ],
-    entry_points={
-        "console_scripts": [
-            # Add your command line scripts here
-        ],
+    packages=find_packages(where="dynamo"),
+    package_dir={"": "dynamo"},
+    install_requires=["requests"],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-mock",
+            "pytest-cov",
+            "moto",
+            "pytest-cov",
+        ]
     },
 )
