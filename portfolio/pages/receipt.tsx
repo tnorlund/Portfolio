53d51da07baa37b3f818234f410b1d8fd2d4042c
Tyler Norlund <tnorlund@icloud.com>
2025-05-21 11:46:01 -0700

    Cra to next (#36)
    * 🔨 Starting the transition
    
    * [portfolio] Migrate CRA to Next.js (#35)

---
+++ portfolio/pages/receipt.tsx
@@ -0,0 +1,5 @@
+import Receipt from '../src/Receipt';
+
+export default function ReceiptPage() {
+  return <Receipt />;
+}
---
+++ portfolio/pages/resume.tsx
@@ -0,0 +1,5 @@
+import Resume from '../src/Resume';
+
+export default function ResumePage() {
+  return <Resume />;
+}
