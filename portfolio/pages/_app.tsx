4529e5860190847932c8051b719c4136a8d18eb2
Tyler Norlund <tnorlund@icloud.com>
2025-05-21 18:08:19 -0700

    Revert "Cra to next (#36)"
    This reverts commit 1b235d58bbccc6030dd9d692695a6288b51fa3e6.

--- portfolio/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../src/index.css';
-import '../src/App.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
