a40f85a3414964292af2e0f8615c4624715bf5e3
Tyler Norlund <tnorlund@icloud.com>
2025-05-31 15:30:28 -0700

    🔨 SSR now working

--- portfolio/components/ui/Figures/index.ts
+++ portfolio/components/ui/Figures/index.ts
@@ -1,10 +1,24 @@
+import dynamic from "next/dynamic";
+
+// Make MerchantCount and LabelValidationCount client-side only to prevent SSR issues during static export
+const ClientMerchantCount = dynamic(() => import("./MerchantCount"), {
+  ssr: false,
+});
+
+const ClientLabelValidationCount = dynamic(
+  () => import("./LabelValidationCount"),
+  {
+    ssr: false,
+  }
+);
+
 export { default as ZDepthConstrained } from "./ZDepthConstrained";
 export { default as ZDepthUnconstrained } from "./ZDepthUnconstrained";
 export { default as UploadDiagram } from "./UploadDiagram";
 export { default as EmbeddingExample } from "./EmbeddingExample";
 export { default as EmbeddingCoordinate } from "./EmbeddingCoordinate";
 export { ClientImageCounts, ClientReceiptCounts } from "./DataCounts";
-export { default as MerchantCount } from "./MerchantCount";
+export { ClientMerchantCount as MerchantCount };
 export { default as ReceiptStack } from "./ReceiptStack";
-export { default as LabelValidationCount } from "./LabelValidationCount";
+export { ClientLabelValidationCount as LabelValidationCount };
 export { default as ReceiptBoundingBox } from "./ReceiptBoundingBox";
