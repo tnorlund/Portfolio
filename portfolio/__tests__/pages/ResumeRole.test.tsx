30dd2e8b804f660c28ddea8e7cd9d4764532307c
Tyler Norlund <tnorlund@icloud.com>
2025-06-22 19:00:28 -0700

    🧹 Cleaned up tests

--- portfolio/__tests__/pages/ResumeRole.test.tsx
+++ portfolio/__tests__/pages/ResumeRole.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { ResumeRole } from "../resume";
+import { ResumeRole } from "../../pages/resume";
 
 describe("ResumeRole", () => {
   const sampleRole = {
