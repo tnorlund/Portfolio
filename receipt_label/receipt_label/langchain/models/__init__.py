79adc72ca263034a1ccbefcd9ba77c4e11208f47
Tyler Norlund <tnorlund@icloud.com>
2025-09-03 08:56:30 -0700

    Add LabelType to currency_validation imports
    LabelType is now imported and included in __all__ to make it available for external use from the models package.

--- receipt_label/receipt_label/langchain/models/__init__.py
+++ receipt_label/receipt_label/langchain/models/__init__.py
@@ -1,4 +1,5 @@
 from .currency_validation import (
+    LabelType,
     CurrencyClassificationItem,
     CurrencyClassificationResponse,
     CurrencyLabel,
@@ -6,6 +7,7 @@ from .currency_validation import (
 )
 
 __all__ = [
+    "LabelType",
     "CurrencyClassificationItem",
     "CurrencyClassificationResponse",
     "CurrencyLabel",
