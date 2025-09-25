36b190516912306d0ed070fbac4021b5d595c037
Tyler Norlund <tnorlund@icloud.com>
2025-09-24 21:30:17 -0700

    Resolve merge conflict in orchestrator.py - remove duplicate EC2 permissions block

--- infra/chroma/orchestrator.py
+++ infra/chroma/orchestrator.py
@@ -157,17 +157,6 @@ class ChromaOrchestrator(pulumi.ComponentResource):
                             ],
                             "Resource": "*",
                         },
-                        # EC2 permissions for NAT instance start/stop/describe (resource must be "*")
-                        {
-                            "Effect": "Allow",
-                            "Action": [
-                                "ec2:StartInstances",
-                                "ec2:StopInstances",
-                                "ec2:DescribeInstanceStatus",
-                                "ec2:DescribeInstances",
-                            ],
-                            "Resource": "*",
-                        },
                     ],
                 }
             )
