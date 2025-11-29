3a6e6c65954fb0c88f43c8456a5b396e18ac28bc
Tyler Norlund <tnorlund@icloud.com>
2025-11-29 10:59:57 -0800

    feat: Add Cursor logo component (#475)
    - Add CursorLogo component following existing logo pattern
    - Use var(--text-color) for light/dark mode support
    - Export CursorLogo from Logos index
    - Matches structure and styling of other logo components

--- portfolio/components/ui/Logos/index.tsx
+++ portfolio/components/ui/Logos/index.tsx
@@ -14,3 +14,4 @@ export { default as LangChainLogo } from "./LangChainLogo";
 export { default as DockerLogo } from "./DockerLogo";
 export { default as ChromaLogo } from "./ChromaLogo";
 export { default as GithubActionsLogo } from "./GithubActionsLogo";
+export { default as CursorLogo } from "./CursorLogo";
